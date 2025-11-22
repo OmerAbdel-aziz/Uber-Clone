88ab8df2a0321ad66c01666d72eef22315d8079c
OmerAbdel-aziz <omercode3@gmail.com>
2025-11-22 21:22:59 +0300

    initial commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,54 +1,15 @@
-import {
-  Inter_400Regular,
-  Inter_500Medium,
-  Inter_600SemiBold,
-  Inter_700Bold,
-  useFonts,
-} from '@expo-google-fonts/inter';
-import { Slot } from 'expo-router';
+import { Stack } from "expo-router";
 import { HeroUINativeProvider } from 'heroui-native';
 import { StyleSheet } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { KeyboardProvider } from 'react-native-keyboard-controller';
-import {
-  configureReanimatedLogger,
-  ReanimatedLogLevel,
-} from 'react-native-reanimated';
 import '../../global.css';
-import { AppThemeProvider } from '../contexts/app-theme-context';
 
-configureReanimatedLogger({
-  level: ReanimatedLogLevel.warn,
-  strict: false,
-});
-
-export default function Layout() {
-  const fonts = useFonts({
-    Inter_400Regular,
-    Inter_500Medium,
-    Inter_600SemiBold,
-    Inter_700Bold,
-  });
-
-  if (!fonts) {
-    return null;
-  }
-
+export default function RootLayout() {
   return (
     <GestureHandlerRootView style={styles.root}>
-      <KeyboardProvider>
-        <AppThemeProvider>
-          <HeroUINativeProvider
-            config={{
-              textProps: {
-                allowFontScaling: false,
-              },
-            }}
-          >
-            <Slot />
-          </HeroUINativeProvider>
-        </AppThemeProvider>
-      </KeyboardProvider>
+      <HeroUINativeProvider>
+        <Stack />
+      </HeroUINativeProvider>
     </GestureHandlerRootView>
   );
 }
--- src/components/showcases/cooking-onboarding/constants.ts
+++
@@ -1,8 +0,0 @@
-import type { PopoverProgressAnimationConfigs } from 'heroui-native';
-
-export const progressAnimationConfigs: PopoverProgressAnimationConfigs = {
-  onClose: {
-    animationType: 'timing',
-    animationConfig: { duration: 100 },
-  },
-};
